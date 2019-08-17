938db2b794aeee9dc7172e469dcf9f933cb1ee94
Kyle Longrich <longrichk@gmail.com>
2019-08-17 18:25:03 -0400

    added a product and test code for it

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,35 @@ pragma solidity ^0.5.0;
 
 contract Marketplace {
     string public name;
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
+
+    struct Product {
+        uint id; 
+        string name;
+        uint price;
+        address owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint id,
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
 
     constructor() public{
         name = "silk road";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+
+        productCount ++;
+        
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,6 @@
 const Marketplace = artifacts.require("./Marketplace.sol")
 
-contract('Marketplace', (accounts) => {
+contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace
 
     before(async () => {
@@ -20,6 +20,25 @@ contract('Marketplace', (accounts) => {
         it('has a name', async () => {
             const name = await marketplace.name()
             assert.equal(name, 'silk road')
+        })
+    })
+
+    describe('products', async() => {
+        let result, productCount
+        
+        before(async () => {
+            result = await marketplace.createProduct('iphonex', web3.utils.toWei('1', 'Ether'), {from: seller})
+            productCount = await marketplace.productCount()
+        })
+
+        it('creates prodcuts', async () => {
+            assert.equal(productCount, 1)
+            const event = result.logs[0].args;
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct')
+            assert.equal(event.name, 'iphonex' ,'name is correct')
+            assert.equal(event.price, web3.utils.toWei('1', 'Ether') ,'Price is correct')
+            assert.equal(event.owner, seller ,'Owner is correct')
+            assert.equal(event.purchased, false ,'Purchased is correct')
         })
     })
 })
