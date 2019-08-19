6f1fd30f65d7351e1d242ee4f33e167963e4be4b
Kyle Longrich <longrichk@gmail.com>
2019-08-19 08:31:11 -0400

    Working market place on the test net! need to fix a few bugs with refreshing. Also want to refactor some of the code as well

--- src/components/App.js
+++ src/components/App.js
@@ -41,6 +41,20 @@ class App extends Component {
       const abi = Marketplace.abi
       const address = Marketplace.networks[networkID].address
       const marketplace = web3.eth.Contract(abi, address)
+
+      this.setState({ marketplace })
+
+      const productCount = await marketplace.methods.productCount().call()
+      this.setState({ productCount })
+      // Load products
+      for (var i = 1; i <= productCount; i++) {
+        const product = await marketplace.methods.products(i).call()
+        this.setState({
+          products: [...this.state.products, product]
+        })
+      }
+      this.setState({ loading: false})
+
     } else {
       window.alert('Marketplace contract not deployed to dectected network')
     }
@@ -53,16 +67,100 @@ class App extends Component {
       account: '',
       productCount: 0,
       products: [],
-      loading: true
+      loading: true,
+      marketplace: ''
     }
+
+    this.purchaseProduct = this.purchaseProduct.bind(this)
+    this.createProduct = this.createProduct.bind(this)
+  }
+
+  createProduct(name, price) {
+    this.state.loading=({loading: true})
+    this.state.marketplace.methods.createProduct(name, price).send({from: this.state.account})
+    .once('receipt', (receipt) => {
+      this.setState({loading: false})
+    })
+  }
+
+  purchaseProduct(id, price) {
+    this.setState({ loading: true })
+    this.state.marketplace.methods.purchaseProduct(id).send({ from: this.state.account, value: price })
+    .once('receipt', (receipt) => {
+      this.setState({ loading: false })
+    })
   }
 
   render() {
     return (
-      <div>
-        <p> silk road </p>
-        <p> Account: {this.state.account} </p>
-      </div>
+      <div id="content">
+      <h1>Add Product</h1>
+      <form onSubmit={(event) => {
+        event.preventDefault()
+        const name = this.productName.value
+        const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether')
+        this.createProduct(name, price)
+      }}>
+        <div className="form-group mr-sm-2">
+          <input
+            id="productName"
+            type="text"
+            ref={(input) => { this.productName = input }}
+            className="form-control"
+            placeholder="Product Name"
+            required />
+        </div>
+        <div className="form-group mr-sm-2">
+          <input
+            id="productPrice"
+            type="text"
+            ref={(input) => { this.productPrice = input }}
+            className="form-control"
+            placeholder="Product Price"
+            required />
+        </div>
+        <button type="submit" className="btn btn-primary">Add Product</button>
+      </form>
+      <p>&nbsp;</p>
+      <h2>Buy Product</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Owner</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody id="productList">
+          { this.state.products.map((product, key) => {
+            return(
+              <tr key={key}>
+                <th scope="row">{product.id.toString()}</th>
+                <td>{product.name}</td>
+                <td>{window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</td>
+                <td>{product.owner}</td>
+                <td>
+                  { !product.purchased
+                    ? <button
+                        name={product.id}
+                        value={product.price}
+                        onClick={(event) => {
+                          this.purchaseProduct(event.target.name, event.target.value)
+                        }}
+                      >
+                        Buy
+                      </button>
+                    : null
+                  }
+                  </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </div>
     );
   }
 }
